59fba109748f0ebaa2ad0df9b741f071b90b690a
Daniel Garcia <danielagarciac02@gmail.com>
2022-05-05 10:44:23 -0500

    another test for invalid flight page

--- flights/tests.py
+++ flights/tests.py
@@ -57,7 +57,7 @@ class FlightTestCase(TestCase):
         # max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
         
         c = Client()
-        response = c.get("/flights/5")
+        response = c.get("/flights/10")
         self.assertEqual(response.status_code, 404)
 
     def test_flight_page_passengers(self):
